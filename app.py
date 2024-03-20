c0c0e52361f1dbb7b908219dc676e33858c270c8
zjeee <wiritipon.t@ku.th>
2024-03-21 06:11:28 +0700

    no handel

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import tkinter as tk
+window = tk.Tk()
+main_frame = tk.Frame(window)
+main_frame.pack(expand=True, pady=20)
+
+
+lbl_name = tk.Label(main_frame,text="°F(fahrenheit)")
+ent_name = tk.Entry(main_frame,text="text",fg="black",width=10)
+
+Ans = tk.Label(main_frame,text="100°C")
+
+ent_name.pack(side=tk.LEFT)
+lbl_name.pack(side=tk.LEFT)
+
+Ans.pack(side=tk.LEFT)
+
+
+window.mainloop()
